a9a4bf0cd1b28625c1f1cb17c1899e413ba99244
Ivan Shchipin <ivan.shchipin@gmail.com>
2023-02-05 15:06:58 +0300

    issue #2 fixed

--- src/screens/shared_components/logo/Logo.tsx
+++ src/screens/shared_components/logo/Logo.tsx
@@ -5,7 +5,10 @@ import s from './Logo.module.scss';
 const Logo = () => {
 	const navigate = useNavigate();
 
-	const goHome = () => navigate('/', { replace: true });
+	const goHome = () => {
+		navigate('/', { replace: true });
+		window.scrollTo(0, 0);
+	};
 
 	return <img className={s.logo} src={logo} alt="Skyrent" onClick={goHome} />;
 };
